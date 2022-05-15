753f0a69d45d0689192b1b030b293e349fe94156
Vladyslav <baskov228322@gmail.com>
2022-05-15 13:00:35 +0300

    gh pages activated

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <div className="main">
-        <BrowserRouter>
+        <BrowserRouter basename="/meal-app">
 
           <Routes>
             <Route path="/" element={<Layout />}>
